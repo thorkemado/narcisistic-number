6a779e852fb3fa8904132704863a4eee68716def
josé Vicente Dochao Fernández <jvdochaofernandez@gmail.com>
2017-11-08 16:24:42 +0100

    CodeWar solution

---
+++ narcisistic number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace narcisistic_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Narcissistic(153);
+
+           
+        }
+
+        public static bool Narcissistic(int value)
+        {
+          var array= value.ToString().Select(x => int.Parse(x.ToString()));
+            int result = 0;
+            int index = array.Count();
+           foreach(int Val in array)
+            {
+                result = result + (int) Math.Pow(Val, index);
+            }
+            return (result==value);
+        }
+    }
+}
